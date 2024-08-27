b5a9d3d13abafd22093aa7f303ade8d176adecd0
Majaz <majazulhaque123@gmail.com>
2024-08-27 21:54:45 +0530

    set sign-in and sign-up screen using clerk to authenticate user

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers:[
+        {
+            domain:"https://communal-starling-68.clerk.accounts.dev",
+            applicationID:"convex",
+        }
+    ]
+}
+
+export default authConfig;
