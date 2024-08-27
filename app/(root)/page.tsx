5c2388da9a4402357fca4b36ffa9515d1f0aff12
Majaz <majazulhaque123@gmail.com>
2024-08-27 19:49:51 +0530

    fetching podcastcard data from constant and show on podcastCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import LeftSidebar from '@/components/LeftSidebar'
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constant'
 import React from 'react'
 
 const Home = () => {
@@ -7,6 +9,11 @@ const Home = () => {
     <div className=' mt-9 flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+        <div className='podcast_grid'>
+        {podcastData.map(({ id, title, description, imgURL }) => (
+          <PodcastCard key={id} imgURL={imgURL} description={description} title={title} podcastId={id} />
+        ))}
+        </div>
       </section>
     </div>
   )
