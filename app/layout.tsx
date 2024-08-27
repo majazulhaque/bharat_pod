ce6d2db224e362d20a584aac1d97af3d9a02535f
Majaz <majazulhaque123@gmail.com>
2024-08-27 20:04:48 +0530

    setup the convex into our project to fetch data from convex end

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./providers/ConvexProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "BharatPod",
   description: "Generate your podcasts using AI",
-  icons:{
-    icon:'/icons/logo.svg'
-  }
+  icons: {
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
