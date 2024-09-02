5d86ab29f4b7bb8c94b7c5f980e2db55710e7c6c
Majaz <majazulhaque123@gmail.com>
2024-09-02 12:40:19 +0530

    right side carousel using embla carousel

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
             {
                 protocol:'https',
                 hostname:'handsome-cobra-246.convex.cloud'
+            },
+            {
+                protocol:'https',
+                hostname:'img.clerk.com'
             }
         ]
     }
