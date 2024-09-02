83a198ece8d030deb7594136e76af2b03bb19ff7
Majaz <majazulhaque123@gmail.com>
2024-09-02 11:36:41 +0530

    completing the no similar page fount

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 
 const Home = () => {
-  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcast);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   return (
     <div className=" mt-9 flex-col gap-9">
       <section className="flex flex-col gap-5">
