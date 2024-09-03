1343b922d9760ea7a24003f62be2be06ae8bed7d
Majaz <majazulhaque123@gmail.com>
2024-09-03 10:36:53 +0530

    build profile page

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+"use client";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constant";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
-    <div>MobileNav</div>
-  )
-}
+    <section>
+      <Sheet>
+        <SheetTrigger>
+          <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <Link
+            href="/"
+            className="flex cursor-pointer items-center gap-1 pb-10 pl-4"
+          >
+            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+            <h1 className="text-24 font-extrabold text-white-1 ml-2">
+              BharatPod
+            </h1>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav className="flex flex-col h-full gap-6 text-white-1">
+                {sidebarLinks.map(({ route, label, imgURL }) => {
+                  const isActive =
+                    pathname === route || pathname.startsWith(`${route}/`);
+                  return (
+                    <SheetClose asChild key={route}>
+                      <Link
+                        href={route}
+                        key={label}
+                        className={cn(
+                          "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
+                          {
+                            "bg-nav-focus border-r-4 border-orange-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={imgURL}
+                          alt={label}
+                          width={24}
+                          height={24}
+                        />
+                        <p>{label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
